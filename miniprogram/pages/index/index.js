e3d2d3c52b6e518a841f920958942bdf115242ab
Cila <cilacdn@gmail.com>
2019-12-14 12:57:44 +0800

    Remove unused files

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,120 +1,106 @@
 //index.js
+//获取应用实例
 const app = getApp()
-
+const db = wx.cloud.database()
+var product_arr = []
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
+    motto: '',
     userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    products:[],
+    pirze_info:"none"
   },
-
-  onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
+  onLoad: function () {
+    var obj = this;
+    db.collection("products").orderBy('Probability', 'asc').get({
+      success: function (res) {
+        product_arr = res['data']
+        obj.setData({
+          products: res['data']
+        })
+      },
+      fail: console.error
+    })
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true,
+        pirze_info: "block"
       })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+          pirze_info: "block"
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+            pirze_info: "block"
           })
         }
-      }
-    })
-  },
-
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
       })
     }
+    
   },
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true,
+      pirze_info: "block"
     })
   },
-
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
+  addproduct:function(e){
+    var pro = { "pro_name": "我要抽检的商品哟", "Probability": 20, "pro_num": 20, "pro_image": "../image/31.jpg","pro_depict":"一等奖哦"};
+    db.collection("products").add({
+      data: pro,
       success: function (res) {
-
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
+        console.log(res);
       },
-      fail: e => {
-        console.error(e)
-      }
+      fail: console.error
     })
   },
-
+  prize:function(){
+    this.randprize(product_arr);
+  },
+  randprize:function(arr){
+    var prizeList = [];
+    var len = arr.length;
+    for (var i=0;i<len;i++){
+      var pro = arr[i];
+      var radio = parseInt(pro.Probability);
+      for(var k=0;k<radio;k++){
+        prizeList.push(pro);
+      }
+    }
+    var prize_index = Math.floor(Math.random() * Math.floor(prizeList.length));
+    console.log(prizeList)
+    console.log(prize_index);
+    console.log(prizeList[prize_index]);
+  },
+  imageLoad: function (e) {
+    var _this = this;
+    var $width = e.detail.width,
+      $height = e.detail.height,
+      ratio = $width / $height;
+    var viewWidth = 500,
+      viewHeight = 500 / ratio; 
+    this.setData({
+      imgwidth: viewWidth,
+      imgheight: viewHeight
+    })
+  }
 })
